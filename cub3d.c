508e9827ac6aed7cde718f3965e2f63a66691c18
Cassandre Tirions <ctirions@mi-c2-p4.s19.be>
2021-03-01 12:15:44 +0100

    maj

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 14:38:56 by ctirions          #+#    #+#             */
-/*   Updated: 2021/02/25 14:15:05 by ctirions         ###   ########.fr       */
+/*   Updated: 2021/02/25 15:42:40 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(int argc, char **argv)
 	
 	ft_argc_error(argc);
 	ft_set_param(&param, argv);
-	//ft_draw_map(param);
-	//ft_put_player(param, 0x00FFFFFF);
+	ft_draw_map(param);
+	ft_put_player(param, 0x00FFFFFF);
 	mlx_hook(param.win_ptr, 2, 1L<<0, ft_key_hook, &param);
 	mlx_hook(param.win_ptr, 17, 1L<<17, ft_mlx_close, &param);
 	mlx_loop(param.mlx_ptr);
