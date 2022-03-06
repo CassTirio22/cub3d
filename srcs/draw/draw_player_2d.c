98718a2d0a8faf4592083de466dc1cdead5b1c79
CassTirio22 <cassandre.tirions@yahoo.be>
2022-03-06 12:24:57 +0100

    color NSWE

--- srcs/draw/draw_player_2d.c
+++ srcs/draw/draw_player_2d.c
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 16:53:18 by ctirions          #+#    #+#             */
-/*   Updated: 2022/03/05 16:45:02 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/03/06 12:12:10 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ double	dist_y_to_wall(t_cub *cub, t_player *p1)
 	{
 		side_dist += delta_dist;
 		if (p1->pos[0] + side_dist * dx < 0 || p1->pos[0] + side_dist * dx > ft_strlen(cub->map->map[0]) || p1->pos[1] + side_dist * dy < 0 || p1->pos[1] + side_dist * dy > double_char_len(cub->map->map))
-			return (double_char_len(cub->map->map)); 
+			return (double_char_len(cub->map->map));
 		if (is_wall(p1->pos[0] + side_dist * dx, p1->pos[1] + side_dist * dy, cub))
 			final_len = side_dist;
 	}
@@ -78,12 +78,13 @@ double	dist_y_to_wall(t_cub *cub, t_player *p1)
 
 void	draw_view(t_cub *cub, t_player *p1)
 {
+	double	dist_wall;
 	double	dx;
 	double	dy;
-	double	j;
-	double	dist_wall;
 	int		i;
+	int		j;
 	int		player_view;
+	int		color;
 
 	player_view = cub->var->wall_size / 2;
 	i = -31;
@@ -94,15 +95,23 @@ void	draw_view(t_cub *cub, t_player *p1)
 		dy = 0;
 		p1->angle += i;
 		dist_wall = dist_x_to_wall(cub, p1);
+		color = 0xD20926;
 		if (dist_wall > dist_y_to_wall(cub, p1))
+		{
+			color = 0xFFF033;
 			dist_wall = dist_y_to_wall(cub, p1);
+		}
 		p1->angle -= i;
 		while (++j < cub->var->wall_size * dist_wall && j < 5 * cub->var->wall_size)
 		{
 			dx += cos((p1->angle + i) * (M_PI / 180));
 			dy += sin((p1->angle + i) * (M_PI / 180));
+			if (color == 0xFFF033 && dy < 0)
+				color = 0x0C4392;
+			else if (color == 0xD20926 && dx < 0)
+				color = 0x0BC103;
 			draw_pixel(cub->img, cub->var->wall_size * 5 + player_view + dx, \
-			cub->var->wall_size * 5 + player_view - dy, 0xD20926);
+			cub->var->wall_size * 5 + player_view - dy, color);
 		}
 	}
 }
